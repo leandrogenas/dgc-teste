424955f916fabebdba78eebac02b1b821d2bac64
Leandro Fabri Pereira <leandro.fabri96@gmail.com>
2021-01-04 06:20:57 -0300

    testes

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-//exec("box86 /home/pi/wine-6.0-rc5-x86/bin/wine start /d G:\\DEG\\CBX '../acucbl/WRUN32.EXE' -C CFG-WEB DGXC00A.CBX", (error, stdout, stderr) => {
+const { exec } = require('child_process');
 
+execFile("box86 /home/pi/wine-6.0-rc5-x86/bin/wine start /d G:\\DEG\\CBX '../acucbl/WRUN32.EXE' -C CFG-WEB DGXC00A.CBX", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`error: ${error.message}`);
+    return;
+  }
 
-const { spawn } = require('child_process');
-const ls = spawn('box86', [
-    '/home/pi/wine-6.0-rc5-x86/bin/wine', 
-    "start /d G:\\DEG\\CBX ../acucbl/WRUN32.EXE -C CFG-WEB DGXC00A.CBX",
-]);
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
 
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
-
-ls.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
-});
-
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
+  console.log(`stdout:\n${stdout}`);
 });
