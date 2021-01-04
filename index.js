ea185f15dabd16513a1a57bffac1fc1c200781f4
Leandro Fabri Pereira <leandro.fabri96@gmail.com>
2021-01-04 06:14:40 -0300

    testes

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const { exec } = require('child_process');
+//exec("box86 /home/pi/wine-6.0-rc5-x86/bin/wine start /d G:\\DEG\\CBX '../acucbl/WRUN32.EXE' -C CFG-WEB DGXC00A.CBX", (error, stdout, stderr) => {
 
-exec("box86 /home/pi/wine-6.0-rc5-x86/bin/wine start /d G:\\DEG\\CBX '../acucbl/WRUN32.EXE' -C CFG-WEB DGXC00A.CBX", (error, stdout, stderr) => {
-  if (error) {
-    console.error(`error: ${error.message}`);
-    return;
-  }
 
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-    return;
-  }
+const { spawn } = require('child_process');
+const ls = spawn('box86', [
+    '/home/pi/wine-6.0-rc5-x86/bin/wine', 
+    "start /d G:\\DEG\\CBX ../acucbl/WRUN32.EXE -C CFG-WEB DGXC00A.CBX",
+]);
 
-  console.log(`stdout:\n${stdout}`);
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
 });
