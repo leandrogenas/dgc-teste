7ef1efb7b600afc3b394d170e0440d915d4977b2
Leandro Fabri Pereira <leandro.fabri96@gmail.com>
2021-01-04 05:42:45 -0300

    testes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-exec('box86 /home/pi/wine-6.0-rc5-x86/bin/wine start /d G:/DEG/CBX ../acucbl/WRUN32.EXE CFG-WEB DGXC00A.CBX', (error, stdout, stderr) => {
+exec("box86 /home/pi/wine-6.0-rc5-x86/bin/wine start /d G:\\DEG\\CBX '../acucbl/WRUN32.EXE' -C CFG-WEB DGXC00A.CBX", (error, stdout, stderr) => {
   if (error) {
     console.error(`error: ${error.message}`);
     return;
